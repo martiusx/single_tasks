0b3e24256adc784b7b7e5296a59d221c3ea67f06
martiusx <Maciej.Cis@o2.pl>
2023-03-13 21:09:00 +0100

    react sign-in method with firebase

---
+++ React/firebase-actions/src/config/firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBaBrGcGyWhTc29LtP2EgjhSrGgFxggq1g",
+  authDomain: "task-manager-25a2a.firebaseapp.com",
+  projectId: "task-manager-25a2a",
+  storageBucket: "task-manager-25a2a.appspot.com",
+  messagingSenderId: "450147570494",
+  appId: "1:450147570494:web:0811caef52f5944525126f",
+  measurementId: "G-DHJ4JCN6TZ",
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const googleProvider = new GoogleAuthProvider();
