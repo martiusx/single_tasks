d7e1fb31409d4785571ef07490d02ba34916a1a0
martiusx <Maciej.Cis@o2.pl>
2023-03-14 21:00:10 +0100

    read data from firebase

--- React/firebase-actions/src/App.js
+++ React/firebase-actions/src/App.js
@@ -1,7 +1,29 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import { Auth } from "./components/auth";
+import { db } from "./config/firebase";
+import { getDocs, collection } from "firebase/firestore";
 
 function App() {
+  const [movieList, setMovieList] = useState([]);
+
+  const moviesCollectionRef = collection(db, "movies");
+
+  useEffect(() => {
+    const getMovieList = async () => {
+      try {
+        const data = await getDocs(moviesCollectionRef);
+        const filterData = data.docs.map((doc) => ({
+          ...doc.data(),
+          id: doc.id,
+        }));
+        console.log(filterData);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+    getMovieList();
+  }, []);
   return (
     <div className="App">
       <Auth />
