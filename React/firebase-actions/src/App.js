7d1fadb9d6294af488b59991457643835e5ce2fd
martiusx <Maciej.Cis@o2.pl>
2023-03-14 23:43:03 +0100

    adding and deleting data in firebase

--- React/firebase-actions/src/App.js
+++ React/firebase-actions/src/App.js
@@ -2,31 +2,89 @@ import { useEffect, useState } from "react";
 import "./App.css";
 import { Auth } from "./components/auth";
 import { db } from "./config/firebase";
-import { getDocs, collection } from "firebase/firestore";
+import {
+  getDocs,
+  collection,
+  addDoc,
+  deleteDoc,
+  doc,
+} from "firebase/firestore";
 
 function App() {
   const [movieList, setMovieList] = useState([]);
 
+  const [newMovieTitle, setNewMovieTitle] = useState("");
+  const [newReleaseDate, setNewReleaseDate] = useState(0);
+  const [isNewMovieOscar, setIsNewMovieOscar] = useState(false);
+
   const moviesCollectionRef = collection(db, "movies");
 
+  const getMovieList = async () => {
+    try {
+      const data = await getDocs(moviesCollectionRef);
+      const filteredData = data.docs.map((doc) => ({
+        ...doc.data(),
+        id: doc.id,
+      }));
+      setMovieList(filteredData);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const deleteMovie = async (id) => {
+    const movieDoc = doc(db, "movies", id);
+    await deleteDoc(movieDoc);
+    getMovieList();
+  };
   useEffect(() => {
-    const getMovieList = async () => {
-      try {
-        const data = await getDocs(moviesCollectionRef);
-        const filterData = data.docs.map((doc) => ({
-          ...doc.data(),
-          id: doc.id,
-        }));
-        console.log(filterData);
-      } catch (err) {
-        console.error(err);
-      }
-    };
     getMovieList();
   }, []);
+
+  const onSubmitMovie = async () => {
+    try {
+      await addDoc(moviesCollectionRef, {
+        title: newMovieTitle,
+        releaseDate: newReleaseDate,
+        receivedAnOscar: isNewMovieOscar,
+      });
+      getMovieList();
+    } catch (err) {
+      console.error(err);
+    }
+  };
   return (
     <div className="App">
       <Auth />
+      <div>
+        <input
+          placeholder="Movie title..."
+          onChange={(e) => setNewMovieTitle(e.target.value)}
+        />
+        <input
+          placeholder="Movie date..."
+          type="number"
+          onChange={(e) => setNewReleaseDate(e.target.value)}
+        />
+        <input
+          type="checkbox"
+          checked={isNewMovieOscar}
+          onChange={(e) => setIsNewMovieOscar(e.target.checked)}
+        />
+        <label>Received an Oscar</label>
+        <button onClick={onSubmitMovie}>Submit Movie</button>
+      </div>
+      <div>
+        {movieList.map((movie) => (
+          <div key={movie.id}>
+            <h1 style={{ color: movie.receivedAnOscar ? "green" : "red" }}>
+              {movie.title}
+            </h1>
+            <p> Date: {movie.releaseDate}</p>
+            <button onClick={() => deleteMovie(movie.id)}>Delete</button>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
