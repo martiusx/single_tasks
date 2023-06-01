c5785023a3836e29b72961f00356c5e88f1d784e
martiusx <Maciej.Cis@o2.pl>
2023-06-01 19:45:47 +0200

    Reminder of information and training about the API

---
+++ VanillaJS/API/index.js
@@ -0,0 +1,22 @@
+const PlaceHolderAPI = "https://jsonplaceholder.typicode.com/users";
+
+fetch(PlaceHolderAPI)
+  .then((res) => {
+    if (!res.ok) {
+      throw new Error("Wystąpił błąd sieciowy.");
+    }
+    return res.json();
+  })
+  .then((data) => {
+    const usersList = document.createElement("ul");
+    document.body.appendChild(usersList);
+
+    const users = data.map((user) => user.name);
+
+    users.forEach((el) => {
+      const singleUser = document.createElement("li");
+      singleUser.innerHTML = el;
+      usersList.appendChild(singleUser);
+    });
+  })
+  .catch((err) => console.log(err));
